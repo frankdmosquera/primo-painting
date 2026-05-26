e8b2ba3395501e78ceeb628edf1b4c7d2a3a750d
heguerack <heguer76@gmail.com>
2026-05-26 16:52:43 -0600

    new google tag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -107,6 +107,7 @@ export default function RootLayout({
       /> */}
 
       {/* GOOGLE TAG MANAGER SCRIPT  */}
+
       <Script
         id="gtm-init"
         strategy="afterInteractive"
@@ -116,7 +117,7 @@ export default function RootLayout({
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-5D92BKJS');
+        })(window,document,'script','dataLayer','GTM-18189264054');
        `,
         }}
       />
