4770c2c1616a51cd389ecc948a685248bca11bdb
heguerack <heguer76@gmail.com>
2026-05-27 13:56:48 -0600

    added a new code for the google reviews widget

--- components/GoogleReviews.tsx
+++ components/GoogleReviews.tsx
@@ -1,11 +1,14 @@
-import Script from 'next/script'
+import Script from "next/script";
 
 export default function GoogleReviews() {
   return (
     <>
-     {/* <!-- Elfsight Google Reviews | Primo Painters --> */}
-<Script src="https://elfsightcdn.com/platform.js" async></Script>
-<div className="elfsight-app-b4dc5dae-2081-46a9-aa0c-8e1fd365b865" data-elfsight-app-lazy></div>
+      {/* <!-- Elfsight Google Reviews | Primo Painters --> */}
+      <Script src="https://elfsightcdn.com/platform.js" async></Script>
+      <div
+        className="elfsight-app-9ba5075d-6901-442e-9996-80f98158da65"
+        data-elfsight-app-lazy
+      ></div>
     </>
-  )
+  );
 }
