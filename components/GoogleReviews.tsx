f27940b915d5202da408929381c64170f9555f63
heguerack <heguer76@gmail.com>
2026-07-20 14:23:03 -0600

    swapped google review widgets

--- components/GoogleReviews.tsx
+++ components/GoogleReviews.tsx
@@ -7,7 +7,8 @@ export default function GoogleReviews() {
       <Script src="https://elfsightcdn.com/platform.js" async></Script>
       <div className="">
         <div
-          className="elfsight-app-9ba5075d-6901-442e-9996-80f98158da65 "
+          // className="elfsight-app-9ba5075d-6901-442e-9996-80f98158da65 "
+          className="elfsight-app-f4b8272e-464b-4ae3-a8ff-b3d063c6822e "
           data-elfsight-app-lazy
         ></div>
       </div>
